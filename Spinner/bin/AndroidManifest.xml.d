6e11040d8c27a64f243156d398558e5c8130ac40
PobDragon <dave@joanddave.co.uk>
2012-07-14 20:36:47 +0100

    Better ignore settings.

---
+++ Spinner/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Fragger\workspace\Spinner\bin\AndroidManifest.xml : \
+C:\Users\Fragger\workspace\Spinner\AndroidManifest.xml \
---
+++ Spinner/bin/classes/R.java.d
@@ -0,0 +1,4 @@
+C:\Users\Fragger\workspace\Spinner\gen\com\android\example\spinner\R.java \
+ : C:\Users\Fragger\workspace\Spinner\res\layout\main.xml \
+C:\Users\Fragger\workspace\Spinner\res\values\strings.xml \
+C:\Users\Fragger\workspace\Spinner\bin\AndroidManifest.xml \
