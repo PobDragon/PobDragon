54a25021e70c55fdfa3d4b49aa3ef4869db1069c
PobDragon <dave@joanddave.co.uk>
2012-07-16 22:04:20 +0100

    Ah, you need to sign the build before it can be released. Right, back to debug.

--- com.android.example/bin/com.android.example-debug-unaligned.apk.d
+++ com.android.example/bin/com.android.example-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\Users\Fragger\Documents\GitHub\PobDragon\com.android.example\bin\com.android.example-release-unsigned.apk : \
+C:\Users\Fragger\Documents\GitHub\PobDragon\com.android.example\bin\com.android.example-debug-unaligned.apk : \
 C:\Users\Fragger\Documents\GitHub\PobDragon\com.android.example\bin\com.android.example.ap_ \
 C:\Users\Fragger\Documents\GitHub\PobDragon\com.android.example\bin\classes.dex \
 C:\Program Files (x86)\Android\android-sdk\tools\support\annotations.jar \
--- com.android.example/gen/com/android/example/spinner/BuildConfig.java
+++ com.android.example/gen/com/android/example/spinner/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.android.example.spinner;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = false;
+    public final static boolean DEBUG = true;
 }
